df8b38eedc0aff0539ab7e67353337fe6c3e355a
Maxim Krivchikov <maxim.krivchikov@gmail.com>
2020-04-05 11:01:39 +0300

    map: groundRows is a property

--- performance/map.js
+++ performance/map.js
@@ -1,6 +1,7 @@
 var map = {
     cols: 64,
     rows: 48,
+    groundRows: 20,
     tsize: 16,
     layers: [],
     getTile: function (layer, col, row) {
@@ -18,14 +19,13 @@ function makeRow(layer, tile, cols) {
     }
 }
 
-var groundRows = 20;
 function makeMap(cols, rows) {
     let layer = [];
     makeRow(layer, { x: 3, y: 5, text: "S", color: "white", kind: "sky" }, cols); // "sky"
-    for (let i = 1; i < rows - groundRows; i++) {
+    for (let i = 1; i < rows - map.groundRows; i++) {
         makeRow(layer, { x: 8, y: 1, kind: "air" }, cols)
     }
-    for (let i = rows - groundRows; i < rows; i++) {
+    for (let i = rows - map.groundRows; i < rows; i++) {
         makeRow(layer, { x: 1, y: 1, text: 0, color: "white", kind: "ground" }, cols)
     }
     return layer;
